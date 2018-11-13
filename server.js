173f5cb4822a45f0cfdd1113a15f7d1f47a408c5
Zeke Sikelianos <zeke@sikelianos.com>
2018-11-12 20:20:57 -0800

    display usage if lng/lat are missing from request

--- server.js
+++ server.js
@@ -8,14 +8,23 @@ const port = Number(process.env.PORT) || 5000
 const app = express()
 
 app.get('/*', (req, res, next) => {
+  const {lng, lat} = req.query
+  
+  
+  if (!lng || !lat) {
+    let reply = '`lng` and `lat` query params required.\n'
+    reply += `Try http://localhost:${port}/?lng=-109.82152075444654&lat=33.987415215464544`
+    return res.send(reply)
+  }
   console.log(req.query)
-  const county = whichCounty([req.query.lng, req.query.lat]) || {}
+
+  const county = whichCounty([lng, lat]) || {}
   return res.json(county)
 })
 
 if (!module.parent) {
   app.listen(port, () => {
-    console.log(`app running on http://localhost:${port}`)
+    console.log(`us-counties running on http://localhost:${port}`)
   })
 }
 
